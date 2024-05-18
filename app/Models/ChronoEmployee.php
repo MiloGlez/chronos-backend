a86484fa89eea8cea1f70556a7ea58b01c3ba4cf
unknown <jcvillada0@gmail.com>
2024-05-18 17:55:04 -0500

    add new files models

---
+++ app/Models/ChronoEmployee.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChronoEmployee extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/ChronoTime.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChronoTime extends Model
+{
+    use HasFactory;
+}
