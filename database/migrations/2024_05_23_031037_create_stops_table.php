8ec55602ab0f1774dd47d91ae025ac73f6f34b4f
Camilo González <95052096+MiloGlez@users.noreply.github.com>
2024-05-22 23:34:25 -0500

    Feature/add migration stop (#2)
    * line
    
    * add migration stop

---
+++ database/migrations/2024_05_23_031037_create_stops_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stops', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('employee_id')->references('id')->on('employees');
+            $table->foreignId('status_id')->references('id')->on('statuses');
+            $table->datetime('start_date');
+            $table->datetime('end_date');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stops');
+    }
+};
