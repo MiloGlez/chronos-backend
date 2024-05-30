f79b21e714513f033856f8300b9573a9e606e5e8
Dvn2014 <85833322+Dvn2014@users.noreply.github.com>
2024-05-29 23:06:57 -0500

    Enpoit Statutes

---
+++ app/Repository/Status/EloquentStatusRepository.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Repository\Status;
+
+use App\Models\ChronoStatus;
+use App\Repository\Status\Interfaces\StatusRepositoryInterface;
+
+class EloquentStatusRepository implements StatusRepositoryInterface
+{
+    public function getAll()
+    {
+        return ChronoStatus::all();    
+    }
+}
